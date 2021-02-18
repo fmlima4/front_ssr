07798f217e7f261b3b5a92b0131246c61bcbc24a
Felipe Miranda de Lima <fmlima4@outlook.com>
2021-02-18 16:21:24 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import {Title} from '../styles/pages/Home'
+
+export default function Home() {
+  return (
+    <div> 
+      <Title>hello word</Title>
+    </div>
+  )
+}
